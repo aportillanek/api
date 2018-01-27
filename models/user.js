92997e5f463d518d4ddcb294814b148773e64f54
lcorderof <luiscorderof.developer@gmail.com>
2018-01-27 12:15:52 -0500

    subir cambio validacion mongo

--- models/user.js
+++ models/user.js
@@ -1,9 +1,18 @@
 let con =require('./connect_mongo');
 let user_json={
-  nombre:String,
-  email:String,
-  username:String,
-  password:String
+  nombre:{type:String,required:true,maxlength:[30,"El nombre no debe excede los 30 caracteres"]},
+  email:{type:String,match:[/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,"el email no es valido"]},
+  password:{
+  type:String,
+  minlength:[5,"El password debe contener 5 caracteres o mas"],
+  validate:{
+    validator(pass){
+      return this.password_confirm==pass;
+    },
+    message: "Las contraseñas no coinciden"
+  }
+
+  }
 
 };
 let user_schema=new con.Schema(user_json);
