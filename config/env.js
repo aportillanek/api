73223be56f205a4bdfbde57eda1121c19178b5f5
lcorderof <luiscorderof.developer@gmail.com>
2018-02-03 10:31:40 -0500

    cambiospuerto

--- config/env.js
+++ config/env.js
@@ -1,3 +1,3 @@
-process.env.URL_CORS = "http://localhost:4200";
+process.env.URL_CORS = "http://190.42.157.137:4200";
 process.env.PORT = 3000;
-process.env.URL_CONNECT = 'mongodb://localhost/sistema';
+process.env.URL_CONNECT = 'mongodb://http://149.56.218.195/sistema';
